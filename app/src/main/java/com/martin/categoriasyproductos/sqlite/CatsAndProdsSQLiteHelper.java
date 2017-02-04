c3c9ab6ed4c3fda7275742a11780217ad4854b8b
martinchoC <martin.e.castro.88@gmail.com>
2017-02-04 15:48:47 -0300

    Code indented

--- app/src/main/java/com/martin/categoriasyproductos/sqlite/CatsAndProdsSQLiteHelper.java
+++ app/src/main/java/com/martin/categoriasyproductos/sqlite/CatsAndProdsSQLiteHelper.java
@@ -8,6 +8,7 @@ import android.provider.BaseColumns;
 /**
  * Created by MartinchoC on 2/4/17.
  */
+
 public class CatsAndProdsSQLiteHelper extends SQLiteOpenHelper  {
 
     private static final String DB_NAME = "CatsAndProds.db";
@@ -16,10 +17,11 @@ public class CatsAndProdsSQLiteHelper extends SQLiteOpenHelper  {
     //Categories Table functionality
     public static final String CATEGORIES_TABLE = "CATEGORIES";
     public static final String COLUMN_CATEGORY_TITLE = "TITLE";
+
     private static String CREATE_CATEGORIES =
-            "CREATE TABLE " + CATEGORIES_TABLE +
-            "(" + BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
-            COLUMN_CATEGORY_TITLE + " TEXT)";
+                                                "CREATE TABLE " + CATEGORIES_TABLE +
+                                                "(" + BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                                                COLUMN_CATEGORY_TITLE + " TEXT)";
 
     //Products Table functionality
     public static final String PRODUCTS_TABLE = "PRODUCTS";
@@ -30,15 +32,16 @@ public class CatsAndProdsSQLiteHelper extends SQLiteOpenHelper  {
     public static final String COLUMN_PRODUCT_CREATION = "CREATION";
     public static final String COLUMN_PRODUCT_EXPIRATION = "EXPIRATION";
 
-    private static final String CREATE_PRODUCTS = "CREATE TABLE " + PRODUCTS_TABLE + " ("
-            + BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
-            COLUMN_PRODUCT_TITLE + " TEXT, " +
-            COLUMN_PRODUCT_PRICE + " DOUBLE, " +
-            COLUMN_PRODUCT_STOCK + " INTEGER, " +
-            COLUMN_FOREIGN_KEY_CATEGORY + " INTEGER, " +
-            COLUMN_PRODUCT_CREATION + " DATE, " +
-            COLUMN_PRODUCT_EXPIRATION + " DATE, " +
-            "FOREIGN KEY(" + COLUMN_FOREIGN_KEY_CATEGORY + ") REFERENCES CATEGORIES(_ID))";
+    private static final String CREATE_PRODUCTS =
+                                                "CREATE TABLE " + PRODUCTS_TABLE + " (" +
+                                                BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                                                COLUMN_PRODUCT_TITLE + " TEXT, " +
+                                                COLUMN_PRODUCT_PRICE + " DOUBLE, " +
+                                                COLUMN_PRODUCT_STOCK + " INTEGER, " +
+                                                COLUMN_FOREIGN_KEY_CATEGORY + " INTEGER, " +
+                                                COLUMN_PRODUCT_CREATION + " DATE, " +
+                                                COLUMN_PRODUCT_EXPIRATION + " DATE, " +
+                                                "FOREIGN KEY(" + COLUMN_FOREIGN_KEY_CATEGORY + ") REFERENCES CATEGORIES(_ID))";
 
     public CatsAndProdsSQLiteHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
